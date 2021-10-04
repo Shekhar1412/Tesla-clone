aa117647b72939f9cf171d7bb6f59a55193591e6
Shekhar1412 <30951818+Shekhar1412@users.noreply.github.com>
2021-10-04 16:42:40 +0530

    Create section.js

---
+++ tesla-clone/src/components/section.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function section() {
+    return (
+        <Wrap>
+            
+        </Wrap>
+    )
+}
+
+export default section
+
+const Wrap = styled.div`
+
+`
