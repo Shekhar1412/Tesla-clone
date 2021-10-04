c389b4933125dcd7945ce0c14e35cf7af3b35be5
Shekhar1412 <30951818+Shekhar1412@users.noreply.github.com>
2021-10-04 16:40:52 +0530

    Update home.js

---
+++ tesla-clone/src/components/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function home() {
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
+
+export default home
+
+const Container = styled.div`
+    height: 100vh;
+`
