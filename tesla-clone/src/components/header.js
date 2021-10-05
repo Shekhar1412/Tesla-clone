2bae6bde1d725b72a33ac0f457259a014a68e04c
Shekhar1412 <30951818+Shekhar1412@users.noreply.github.com>
2021-10-05 19:36:13 +0530

    add: header

--- tesla-clone/src/components/header.js
+++ tesla-clone/src/components/header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components';
-
+import MenuIcon from '@mui/icons-material/Menu';
 function header() {
     return (
         <Container>
@@ -8,11 +8,14 @@ function header() {
                 <img src="./images/logo.svg"></img>
             </a>
             <Menu>
-                <a>Model S</a>
-                <a>Model 3</a>
-                <a>Model X</a>
-                <a>Model Y</a>
+                <a href="/">Model S</a>
+                <a href="/">Model 3</a>
+                <a href="/">Model X</a>
+                <a href="/">Model Y</a>
             </Menu>
+            <RightMenu>
+
+            </RightMenu>
         </Container>
     )
 }
@@ -25,8 +28,24 @@ const Container = styled.div`
     display:flex;
     align-items:center;
     padding: 0 20px;
+    top:0;
+    left: 0;
+    right: 0;
 `
 
 const Menu = styled.div`
+    display: flex;
+    align-items:center;
+    justify-content: center;
+    flex: 1;
+    a{
+        font-weight:600;
+        text-transform: uppercase;
+        margin: 10px;
+    }
+    
+`
+
+const RightMenu = styled.div`
 
 `
